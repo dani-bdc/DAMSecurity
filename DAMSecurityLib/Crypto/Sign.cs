53320fc843fd53daf00ad185e2fe6b2d89fc0fc7
Dani Martí <dmart126@boscdelacoma.cat>
2023-12-04 13:03:14 +0100

    Create header of functions to sign

---
+++ DAMSecurityLib/Crypto/Sign.cs
@@ -0,0 +1,21 @@
+﻿using DAMSecurityLib.Certificates;
+using iText.Forms.Fields;
+using iText.Kernel.Pdf;
+using iText.Signatures;
+
+using System;
+
+namespace DAMSecurityLib.Crypto
+{
+    public class Sign
+    {
+        public static void SignPdf(string inputFileName, string outputFileName)
+        {
+        }
+
+        public static void SignImage(string inputFileName, string outputFileName)
+        {
+
+        }
+    }
+}
