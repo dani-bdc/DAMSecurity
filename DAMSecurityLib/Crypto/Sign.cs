0f55da8c65c2127b529f1502242cfdaabea6f7ef
Dani Martí <dmart126@boscdelacoma.cat>
2023-12-04 14:27:06 +0100

    Init Pkcs12Store from pfx

--- DAMSecurityLib/Crypto/Sign.cs
+++ DAMSecurityLib/Crypto/Sign.cs
@@ -2,18 +2,27 @@
 using iText.Forms.Fields;
 using iText.Kernel.Pdf;
 using iText.Signatures;
-
+using Org.BouncyCastle.Pkcs;
 using System;
 
 namespace DAMSecurityLib.Crypto
 {
     public class Sign
     {
-        public static void SignPdf(string inputFileName, string outputFileName)
+        private Pkcs12Store pfxKeyStore = new Pkcs12Store();
+
+
+        public void InitPkcs12StoreFromPfx(string pfxFilePath, string pfxPassword)
         {
+            this.pfxKeyStore = new Pkcs12Store(new FileStream(pfxFilePath, FileMode.Open, FileAccess.Read), pfxPassword.ToCharArray());
         }
 
-        public static void SignImage(string inputFileName, string outputFileName)
+        public  void SignPdf(string inputFileName, string outputFileName)
+        {
+            
+        }
+
+        public  void SignImage(string inputFileName, string outputFileName)
         {
 
         }
