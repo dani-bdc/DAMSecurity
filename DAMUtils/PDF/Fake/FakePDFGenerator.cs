152a5f698c9837559bb8a0e52d249063300ab817
danibdc <dmart126@boscdelacoma.cat>
2024-01-23 17:15:25 +0100

    Add DAMUtils project

---
+++ DAMUtils/PDF/Fake/FakePDFGenerator.cs
@@ -0,0 +1,33 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAMUtils.PDF.Fake
+{
+    public class FakePDFGenerator : IPDFGenerator
+    {
+        byte[] IPDFGenerator.Generate(object prms)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (PdfWriter writer = new PdfWriter(ms))
+                {
+                    using (PdfDocument pdfDocument = new PdfDocument(writer))
+                    {
+                        using (Document document = new Document(pdfDocument))
+                        {
+                            document.Add(new Paragraph("Hello world!"));
+                        }
+                    }
+                }
+                return ms.ToArray();
+            }
+            
+        }
+    }
+}
---
+++ DAMUtils/PDF/IPDFGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAMUtils.PDF
+{
+    public interface IPDFGenerator
+    {
+        public byte[] Generate(object prms);
+    }
+}
